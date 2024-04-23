4df9b73e18e2c1a00529386745a1903fbb6287cc
javid shabin <javid.main.act@gmail.com>
2024-04-23 22:32:39 +0530

    Config the middlewares in main server.ts

--- server.ts
+++ server.ts
@@ -1,15 +1,34 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express, { Request, Response } from 'express';
+import cors from "cors";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import rateLimit from "express-rate-limit";
 import { connectDb } from './src/configs/connectDb';
 
 const app = express();
 const PORT = 5000;
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, 
+  max: 100,
+  message: "Too many requests from this IP, please try again later.",
+});
+
+app.use(limiter);
+app.use(
+  cors({
+    origin: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+app.use(cookieParser());
+app.use(helmet());
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Weclo"Welcome to Delivista!"');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to Zippyzag!");
 });
 
 connectDb()
