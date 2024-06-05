90f9aa329946f262325dcb35b9bd733a3815fdca
javid shabin <javid.main.act@gmail.com>
2024-06-05 20:07:43 +0530

    Complete the OTP generation and sending funciton store the data in temp user schema

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,21 +1,47 @@
 import { Request, Response, NextFunction } from "express";
-import tempUserSchema from "./user.tempModel"
-import userSchema from "./user.model"
+import tempUserSchema from "./user.tempModel";
+import userSchema from "./user.model";
 import { IuserCreaction } from "./user.interface";
-import {hashPassword, sendOtpEmail} from "./user.service"
+import { hashPassword, sendOtpEmail } from "./user.service";
 import { AppError } from "../../utils/appError";
 import { validateSignupUser } from "./user.validation";
 
-
 // Generate and send OTP to user email
-export const signupUser = (req: Request, res: Response, next: NextFunction) => {
-   try {
+export const signupUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
     // Validate the user details in validator function
     validateSignupUser(req.body);
-    const { name, email, password, confirmPassword, phone} = req.body;
-   } catch (error) {
-    
-   }
+    const { name, email, password, confirmPassword, phone } = req.body;
+    // Check the user is already exist in database
+    const existingUser = await userSchema.findOne({ email });
+    if (existingUser) return next(new AppError("User already exists", 400));
+
+    // Hash the user password
+    const hashedPassword = await hashPassword(password);
+    // Generate the 6 digit OTP using math function
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+    // Create the tempUser
+    await tempUserSchema.findOneAndUpdate(
+      { email },
+      {
+        name,
+        email,
+        password: hashedPassword, // add the hash password
+        phone,
+        otp,
+        otpExpires: new Date(Date.now() + 10 * 60000), // add the expire data for OTP
+      },
+      { upsert: true, new: true }
+    );
+    await sendOtpEmail(email, otp);
+    res.status(200).json({ message: "OTP sent to email" });
+  } catch (error) {
+    next(error);
+  }
 };
 // Verify the OTP and create a new user
 export const verifyOtpAndCreateUser = (req: Request, res: Response) => {};
