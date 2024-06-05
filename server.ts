d27ec000d25ef9eb810dbd3a2c1536b854a5e682
javid shabin <javid.main.act@gmail.com>
2024-06-05 18:56:15 +0530

    create app.ts for handle apis perfectly

--- server.ts
+++ server.ts
@@ -8,7 +8,7 @@ import cookieParser from "cookie-parser";
 import rateLimit from "express-rate-limit";
 import { connectDb } from "./src/configs/connectDb";
 
-const app = express();
+const server = express();
 const PORT = 5000; // Server listening port
 
 // Rate limiter configuration: limit each IP to 100 requests per 15 minutes
@@ -17,22 +17,22 @@ const limiter = rateLimit({
   max: 100, // max requests per IP
   message: "Too many requests from this IP, please try again later.",
 });
-app.use(limiter); // Apply rate limiting middleware globally
+server.use(limiter); // Apply rate limiting middleware globally
 
 // Enable CORS for specific origins and restrict allowed HTTP methods
-app.use(
+server.use(
   cors({
     origin: true, // Only allow requests from this origin
     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
   })
 );
 
-app.use(cookieParser()); // Parse cookies attached to client requests
-app.use(helmet()); // Secure HTTP headers to protect against common vulnerabilities
-app.use(express.json()); // Parse incoming JSON payloads into JavaScript objects
+server.use(cookieParser()); // Parse cookies attached to client requests
+server.use(helmet()); // Secure HTTP headers to protect against common vulnerabilities
+server.use(express.json()); // Parse incoming JSON payloads into JavaScript objects
 
 // Root route for server health check
-app.get("/", (req: Request, res: Response) => {
+server.get("/", (req: Request, res: Response) => {
   res.send("Welcome to Zippyzag!");
 });
 
@@ -40,7 +40,7 @@ app.get("/", (req: Request, res: Response) => {
 connectDb()
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
   })
