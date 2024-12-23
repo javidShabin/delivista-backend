498a7ac953c4867562508f44296252e0cdba5416
javid shabin <javid.main.act@gmail.com>
2024-12-23 12:29:09 +0530

    create the fuction for get all users

--- controllers/userController.js
+++ controllers/userController.js
@@ -183,6 +183,23 @@ const userLogin = async (req, res) => {
     res.status(500).json({ message: "Failed to login user" });
   }
 };
+// Get all user list from database
+const getAllUsers = async (req, res) => {
+  try {
+    // Fetch all users from the database
+    const users = await User.find();
+
+    if (!users) {
+      return res.status(400).json({ message: "Useres not availbale" });
+    }
+
+    // Respond with the list of users
+    res.status(200).json(users);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).json({ message: "Failed to fetch users. Please try again later." });
+  }
+};
 
 // Export the functions correctly
-export { userRegistration, verifyOtpAndCreateUser, userLogin };
+export { userRegistration, verifyOtpAndCreateUser, userLogin, getAllUsers };
--- routes/v1/userRouter.js
+++ routes/v1/userRouter.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+  getAllUsers,
   userLogin,
   userRegistration,
   verifyOtpAndCreateUser,
@@ -10,5 +11,6 @@ const router = express.Router();
 router.post("/signup", userRegistration); // Use userRegistration as the handler for POST /signup
 router.post("/verify-otp", verifyOtpAndCreateUser); // Otp verification haldler for pot
 router.post("/login-user", userLogin); // user login handler for post request
+router.get("/get-user-list", getAllUsers) // Get user list handler get all useres from the database
 
 export const userRouter = router;
