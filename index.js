6bc23ad4f96dc9e661dd42d441ccab34d0bdd6eb
javid shabin <javid.main.act@gmail.com>
2024-12-23 12:48:36 +0530

    install compression for optimize backend perfomence

--- index.js
+++ index.js
@@ -2,34 +2,42 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
-import helmet from 'helmet';
+import helmet from "helmet";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import compression from "compression"; // Import compression
 import { connectDb } from "./config/db.js";
 import { apiRouter } from "./routes/index.js";
-
-
 
 const app = express();
 const port = process.env.PORT || 5000; // Use PORT from .env if available
 
-connectDb()
+connectDb();
 
 // Middleware
-app.use(helmet())
-app.use(cors());
+app.use(helmet());
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  compression({
+    threshold: 1024, // Only compress responses larger than 1KB
+    level: 6, // Compression level (1-9)
+  })
+);
 
 // Api router
-app.use("/api",apiRouter)
+app.use("/api", apiRouter);
 
 // Default route
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
-
-
 
 // Start the server
 app.listen(port, () => {
