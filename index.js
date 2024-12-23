6bf212613687d9cbd9441c0395392d229c6a6919
javid shabin <javid.main.act@gmail.com>
2024-12-23 12:35:45 +0530

    install the helmet middleware and make more secure my server

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import helmet from 'helmet';
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { connectDb } from "./config/db.js";
@@ -15,6 +16,7 @@ const port = process.env.PORT || 5000; // Use PORT from .env if available
 connectDb()
 
 // Middleware
+app.use(helmet())
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
