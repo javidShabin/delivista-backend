c2e99d1dcbfb36a7e26d2ec47a7a099217c7c9f5
javid shabin <javid.main.act@gmail.com>
2024-12-26 23:07:53 +0530

    create the seller controllers body, and export

---
+++ controllers/sellerController.js
@@ -0,0 +1,45 @@
+// Register seller
+const registerSeller = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+// Login the seller
+const loginSeller = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+// Logout the seller, (clear the cookie)
+const logoutSeller = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+// Get seller profile
+const sellerProfile = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+// Update the seller profile include the cloudinery and multer
+const updateSellerProfile = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+// Change the seller password
+const changePassword = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+// Check admin autharization
+const checkSeller = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+export {
+  registerSeller,
+  loginSeller,
+  logoutSeller,
+  sellerProfile,
+  updateSellerProfile,
+  changePassword,
+  checkSeller,
+};
