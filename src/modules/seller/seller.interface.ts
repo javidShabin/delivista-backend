e30d537737051a1db1960f4cda17854fa1c51114
javid shabin <javid.main.act@gmail.com>
2025-01-13 18:53:20 +0530

    Create interface for temp seller schema

--- src/modules/seller/seller.interface.ts
+++ src/modules/seller/seller.interface.ts
@@ -1,12 +1,28 @@
+// Seller main schema interface
 export interface ISeller {
-    name: string;
-    email: string;
-    password: string;
-    phone: string;
-    role: "seller";
-    avatar?: {
-        public_id: string;
-        url: string;
-    };
-    createdAt: Date;
+  name: string;
+  email: string;
+  password: string;
+  phone: string;
+  role: "seller";
+  avatar?: {
+    public_id: string;
+    url: string;
+  };
+  createdAt: Date;
+}
+// Temp seller schema interface
+export interface ITempSeller {
+  name: string;
+  email: string;
+  password: string;
+  phone: string;
+  otp: string;
+  otpExpires: Date;
+  role: "seller";
+  avatar?: {
+    public_id: string;
+    url: string;
+  };
+  createdAt: Date;
 }
