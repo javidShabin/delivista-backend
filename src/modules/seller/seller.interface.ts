cccd7fdf293c2fd00c263da56de95d42b1e27eba
javid shabin <javid.main.act@gmail.com>
2025-01-22 21:27:55 +0530

    Create seller password update interface

--- src/modules/seller/seller.interface.ts
+++ src/modules/seller/seller.interface.ts
@@ -46,3 +46,9 @@ export interface IsellerLogin {
   email: string;
   password: string;
 }
+// Password updating interface
+export interface IupdateSellerPassword {
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
