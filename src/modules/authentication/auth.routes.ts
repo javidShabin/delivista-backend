7bc3f6e07c344c703ac327b3b3be9e8ca6bef151
javid shabin <javid.main.act@gmail.com>
2025-01-27 22:59:00 +0530

    set router for login auth and test it that is working well

--- src/modules/authentication/auth.routes.ts
+++ src/modules/authentication/auth.routes.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import { authenticate } from "../../middlewares/auth.middleware";
 import { authorize } from "../../middlewares/authorize";
-import { singupUser, verifyOtpandCreateUser } from "./auth.controller";
+import { loginUser, singupUser, verifyOtpandCreateUser } from "./auth.controller";
 
 const router = express.Router();
 
 router.post("/user-signup", singupUser)
 
 router.post("/verify-user-otp", verifyOtpandCreateUser)
+
+router.post("/user-login", loginUser)
 
 export default router;
