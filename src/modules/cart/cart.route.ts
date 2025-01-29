f87d0822a01095e33cb9a60f3365aba2c7c730c0
javid shabin <javid.main.act@gmail.com>
2025-01-30 00:22:33 +0530

    Complete the update cart funciton and setup the route for it complete the test by postman

--- src/modules/cart/cart.route.ts
+++ src/modules/cart/cart.route.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import { authenticate } from "../../middlewares/auth.middleware";
 import { authorize } from "../../middlewares/authorize";
-import { addToCart, deleteFromCart, getCartByUserId } from "./cart.controller";
+import { addToCart, deleteFromCart, getCartByUserId, updateCart } from "./cart.controller";
 
 const router = express.Router();
 
 router.post("/add-cart", authenticate, authorize("customer"), addToCart);
 router.get("/cart-items", authenticate, authorize("customer"), getCartByUserId);
+router.put("/update-cart", authenticate, authorize("customer"), updateCart)
 router.delete(
   "/remove-item",
   authenticate,
