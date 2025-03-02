411a665f35a54d4f27b78c4d150c499af3267c50
javid shabin <javid.main.act@gmail.com>
2025-03-02 13:58:04 +0530

    Create schema and model for menu extendes menu interface

---
+++ src/modules/menu/menu.model.ts
@@ -0,0 +1,82 @@
+import mongoose, { Schema, Document } from "mongoose";
+import { IMenu } from "./menu.interface";
+
+// Combines interface with mongoose's Document
+export interface IMenuModel extends IMenu, Document {}
+
+const menuSchema: Schema<IMenuModel> = new Schema(
+  {
+    productName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    restaurantId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Restaurant",
+      required: true,
+    },
+    sellerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Seller",
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    variants: [
+      {
+        name: { type: String, required: true },
+        price: { type: Number, required: true },
+      },
+    ],
+    image: {
+      type: String,
+      default: "",
+    },
+    isAvailable: {
+      type: Boolean,
+      default: true,
+    },
+    isVeg: {
+      type: Boolean,
+      default: false,
+    },
+    isRecommended: {
+      type: Boolean,
+      default: false,
+    },
+    ratings: {
+      type: Number,
+      default: 0.0,
+    },
+    totalReviews: {
+      type: Number,
+      default: 0,
+    },
+    tags: [
+      {
+        type: String,
+        required: true,
+        trim: true,
+      },
+    ],
+    totalOrders: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+// Create the menu model
+export default mongoose.model<IMenuModel>("Menu", menuSchema);
