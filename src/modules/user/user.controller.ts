23aa31fb4a514a4787378e8be62ef8dd92c1bb29
javid shabin <javid.main.act@gmail.com>
2025-03-15 16:23:56 +0530

    Create needed functions bodys in user.controller.ts

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,19 @@
+
+// Generate and send OTP to user email
+export const generateOtp = (req: Request, res: Response) => {};
+// Verify the OTP and create a new user
+export const verifyOtpAndCreateUser = (req: Request, res: Response) => {};
+// Log in the user
+export const loginUser = (req: Request, res: Response) => {};
+// Get users list
+export const getAllUsers = (req: Request, res: Response) => {};
+// Get user by user ID
+export const getUserById = (req: Request, res: Response) => {};
+// Get user profile using user ID
+export const getUserProfileById = (req: Request, res: Response) => {};
+// Update user profile by user ID
+export const updateUserProfile = (req: Request, res: Response) => {};
+// Forgot password
+export const forgotPassword = (req: Request, res: Response) => {};
+// Log out user
+export const logoutUser = (req: Request, res: Response) => {};
