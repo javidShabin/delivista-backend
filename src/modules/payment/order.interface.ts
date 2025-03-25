6866b0879e5b371923180d1924cf9fdd8b3033dd
javid shabin <javid.main.act@gmail.com>
2025-03-25 19:48:22 +0530

    Create the interface for create the oderschema add needed all fields and export

---
+++ src/modules/payment/order.interface.ts
@@ -0,0 +1,40 @@
+import { Types } from "mongoose";
+
+// Order schema interface
+export interface IOrder {
+    customerId: Types.ObjectId;
+    sessionId: string;
+
+    // Address
+    addressId: Types.ObjectId;
+    fullName: string;
+    address: string;
+    city: string;
+    phoneNumber: string;
+    addressType: string;
+
+    // Items in the order
+    items: {
+        menuId: Types.ObjectId;
+        productName: string;
+        quantity: number;
+        image: string;
+        isVeg: boolean;
+        tags: string[];
+        category: string;
+        price: number;
+    }[];
+
+    // Pricing
+    totalAmount: number;
+
+    // Payment & order status
+    paymentStatus: "pending" | "success" | "failed";
+    paymentMethod?: "card" | "upi" | "netbanking" | "cod";
+    transactionId?: string;
+    orderStatus: "placed" | "confirmed" | "shipped" | "delivered" | "cancelled";
+
+    // Metadata
+    createdAt?: Date;
+    updatedAt?: Date;
+}
