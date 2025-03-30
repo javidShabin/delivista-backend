a00dbca6b182178364a060fcdcb832db0a5a6efa
javid shabin <javid.main.act@gmail.com>
2025-03-31 02:28:31 +0530

    create and config the user model and schema, mentioned the all required filed and export the model

---
+++ models/address.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const addressSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  phone: { type: String, required: true },
+  street: { type: String, required: true },
+  city: { type: String, required: true },
+  postalCode: { type: String, required: true },
+  country: { type: String, required: true },
+});
+
+const Address = mongoose.model("address", addressSchema)
+export {Address}
