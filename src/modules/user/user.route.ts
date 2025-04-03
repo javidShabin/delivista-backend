c7efbc04f61a7ac3e873bfbbebe71829d8f670eb
javid shabin <javid.main.act@gmail.com>
2025-04-03 19:49:38 +0530

    Set route for the customer profile editing function

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -2,7 +2,11 @@ import express from "express";
 import { authenticate } from "../../middlewares/auth.middleware";
 import { authorize } from "../../middlewares/authorize";
 import { upload } from "../../middlewares/multer";
-import { getAllCustomer, getCustomerProfile } from "./user.controller";
+import {
+  getAllCustomer,
+  getCustomerProfile,
+  updateCustomerProfile,
+} from "./user.controller";
 
 const router = express.Router();
 
@@ -18,6 +22,14 @@ router.get(
   authenticate,
   authorize("customer"),
   getCustomerProfile
+);
+
+router.put(
+  "/update-customer-profile",
+  authenticate,
+  authorize("customer"),
+  upload.single("avatar"),
+  updateCustomerProfile
 );
 
 export default router;
