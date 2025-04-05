f50be02d349f7ca2c78bb699e731d8542d4a1726
javid shabin <javid.main.act@gmail.com>
2025-04-05 14:50:56 +0530

    Create the order controller inside the pyment module for handle order functions

---
+++ src/modules/payment/order.controller.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from "express";
+import orderSchema from "./order.model"
+
+// Get all orders for customer
+export const orderListByCustomer = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+// Cancel the order
+export const orderCancel = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+// Get orders for restaurants
+export const orderListBySeller = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
