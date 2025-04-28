ea39483468e477fa047e396e9030a0cb5f7af995
javid shabin <javid.main.act@gmail.com>
2025-04-29 01:37:44 +0530

    Set the router for verify admin otp function and test the endpoint using postman, it working well

--- src/modules/admin/admin.route.ts
+++ src/modules/admin/admin.route.ts
@@ -2,10 +2,12 @@ import express from "express";
 import { authenticate } from "../../middlewares/auth.middleware";
 import { authorize } from "../../middlewares/authorize";
 import { upload } from "../../middlewares/multer";
-import { signupAdmin } from "./admin.controller";
+import { signupAdmin, verifyAdminOTP } from "./admin.controller";
 const router = express.Router();
 
 // Admin signup
 router.post("/admin-signup", signupAdmin)
+// Verify admin OTP
+router.post("/verify-admin-otp", verifyAdminOTP);
 
 export default router;
