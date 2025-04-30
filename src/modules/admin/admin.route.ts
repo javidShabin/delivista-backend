9d8e14c3c622959e4bc051afbe93a00aebaca3f4
javid shabin <javid.main.act@gmail.com>
2025-05-01 01:47:08 +0530

    Set the router for the admin login controller function and test the end point using post man, its working well

--- src/modules/admin/admin.route.ts
+++ src/modules/admin/admin.route.ts
@@ -2,12 +2,14 @@ import express from "express";
 import { authenticate } from "../../middlewares/auth.middleware";
 import { authorize } from "../../middlewares/authorize";
 import { upload } from "../../middlewares/multer";
-import { signupAdmin, verifyAdminOTP } from "./admin.controller";
+import { loginAdmin, signupAdmin, verifyAdminOTP } from "./admin.controller";
 const router = express.Router();
 
 // Admin signup
-router.post("/admin-signup", signupAdmin)
+router.post("/admin-signup", signupAdmin);
 // Verify admin OTP
 router.post("/verify-admin-otp", verifyAdminOTP);
+// Admin login
+router.post("/admin-login", loginAdmin);
 
 export default router;
