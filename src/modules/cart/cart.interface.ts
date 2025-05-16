a44ec852317d73db87373f68df6ead2d30d589e3
javid shabin <javid.main.act@gmail.com>
2025-05-16 15:20:41 +0530

    Create cart file in cat moudle and create interface for car item, create interface for main shcema

---
+++ src/modules/cart/cart.interface.ts
@@ -0,0 +1,23 @@
+import { Types } from "mongoose";
+
+// Subschema for cart items
+interface ICartItem {
+  menuItem: Types.ObjectId;
+  quantity: number;
+  price: number;
+  image?: string;
+  productName: string;
+  category: string;
+  isVeg: boolean;
+}
+
+// Cart main schema interface
+export interface ICart {
+  sellerId: Types.ObjectId;
+  customerId: Types.ObjectId;
+  restaurantId: Types.ObjectId;
+  items: ICartItem[];
+  totalPrice: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
