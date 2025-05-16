02ffdc80d66dd2e2f207ae75db53fc8646f16334
javid shabin <javid.main.act@gmail.com>
2025-05-16 15:44:07 +0530

    Update the index.ts and route in cart module and import the cartRouter in app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { restRouter } from "./modules/restaurant";
 import { authRouter } from "./modules/authentication";
 import { userRouter } from "./modules/user";
 import { menuRouter } from "./modules/menu";
+import { cartRouter } from "./modules/cart";
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use("/authentication", authRouter);
 app.use("/user", userRouter);
 app.use("/restaurant", restRouter);
 app.use("/menu", menuRouter)
+app.use("/cart", cartRouter)
 
 // Global error handler
 app.use(globalErrorHandler);
---
+++ src/modules/cart/index.ts
@@ -0,0 +1,11 @@
+import cartRouter from "./cart.route";
+import * as cartController from "./cart.controller";
+import * as cartModel from "./cart.model";
+import * as cartInterfaces from "./cart.interface";
+
+export {
+    cartRouter,
+    cartController,
+    cartModel,
+    cartInterfaces
+};
