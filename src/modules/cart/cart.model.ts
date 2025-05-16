e2f254439e422c7a4c005070dad1d405b079d65e
javid shabin <javid.main.act@gmail.com>
2025-05-16 15:21:56 +0530

    Create cart model and schema on the interfaces

---
+++ src/modules/cart/cart.model.ts
@@ -0,0 +1,76 @@
+import mongoose, { Schema, Document, model } from "mongoose";
+import { ICart } from "./cart.interface";
+
+export interface ICartModel extends ICart, Document {}
+
+// Subschema for items
+const cartItemSchema = new Schema<ICart["items"][0]>(
+  {
+    menuItem: {
+      type: Schema.Types.ObjectId,
+      ref: "MenuItem",
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    image: {
+      type: String,
+    },
+    productName: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    isVeg: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  { _id: false }
+);
+
+// Main Cart schema
+const cartSchema = new Schema<ICartModel>(
+  {
+    sellerId: {
+      type: Schema.Types.ObjectId,
+      ref: "Seller",
+      required: true,
+    },
+    customerId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    restaurantId: {
+      type: Schema.Types.ObjectId,
+      ref: "Restaurant",
+      required: true,
+    },
+    items: {
+      type: [cartItemSchema],
+      required: true,
+    },
+    totalPrice: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Create model
+export default model<ICartModel>("Cart", cartSchema);
