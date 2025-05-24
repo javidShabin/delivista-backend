5d805a75e7c8d832e821a8c122a3aa3a6d62bb69
javid shabin <javid.main.act@gmail.com>
2025-05-24 08:39:46 +0530

    Create validation function in  auth.validation function for singup validation

---
+++ src/modules/authentication/auth.validation.ts
@@ -0,0 +1,23 @@
+import { AppError } from "../../utils/appError";
+import { IuserCreaction } from "./auth.interface";
+
+// Validation for user signupn
+export const validateSignupUser = (data: IuserCreaction) => {
+  // Destructer the user details as data from interface
+  const { name, email, password, confirmPassword, phone } = data;
+  // Check the required fields are present or not
+  if (!name || !email || !password || !confirmPassword || !phone) {
+    throw new AppError("All fields are required", 400);
+  }
+  // Compare the password and conform password
+  if (password !== confirmPassword) {
+    throw new AppError("Passwords do not match", 400);
+  }
+  if (!/^\S+@\S+\.\S+$/.test(email)) {
+    throw new AppError("Invalid email format", 400);
+  }
+
+  if (password.length < 6) {
+    throw new AppError("Password must be at least 6 characters", 400);
+  }
+};
