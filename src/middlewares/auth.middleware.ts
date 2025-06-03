4169695661109775a9bf1c10e962e4bdefb32f1f
javid shabin <javid.main.act@gmail.com>
2025-06-03 19:49:24 +0530

    Create the authmiddleware inside the middleware folder

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,1 +1,40 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { AppError } from "../utils/appError";
+const JWT_SECRET = process.env.JWT_SECRET_KEY || "default_dev_secret";
 
+interface TokenPayload {
+  id: string;
+  email: string;
+  role: "customer" | "admin" | "seller";
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: TokenPayload;
+    }
+  }
+}
+
+export const authenticate = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return next(new AppError("Authentication token missing or invalid", 401));
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET) as TokenPayload;
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+};
