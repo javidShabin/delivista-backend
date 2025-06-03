c9b6622046c437873fee6cae2b7ffbee02f7726e
javid shabin <javid.main.act@gmail.com>
2025-06-03 15:34:26 +0530

    Create the cloudinary for store the image as a link

---
+++ src/configs/cloudinary.ts
@@ -0,0 +1,14 @@
+import { v2 as cloudinary, ConfigOptions } from "cloudinary";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const cloudinaryConfig: ConfigOptions = {
+  cloud_name: process.env.CLOUD_NAME || "",
+  api_key: process.env.CLOUD_API_KEY || "",
+  api_secret: process.env.CLOUD_API_SECRET || "",
+};
+
+cloudinary.config(cloudinaryConfig);
+
+export default cloudinary;
