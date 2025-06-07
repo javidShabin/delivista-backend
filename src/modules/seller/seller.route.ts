06f4425a3eb1743fa06b98b97b0fee7cf6cc78b9
javid shabin <javid.main.act@gmail.com>
2025-06-08 00:58:03 +0530

    Set the seller signup contoller funciton router

--- src/modules/seller/seller.route.ts
+++ src/modules/seller/seller.route.ts
@@ -1,4 +1,11 @@
 import express from "express";
+import { authenticate } from "../../middlewares/auth.middleware";
+import { authorize } from "../../middlewares/authorize";
+import { upload } from "../../middlewares/multer";
+import { signupSeller } from "./seller.controller";
 
 const router = express.Router();
+
+// Seller signup
+router.post("/seller-signup", signupSeller)
 export default router;
