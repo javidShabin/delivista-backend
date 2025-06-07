94b6c98d7ec76c755f98434cf6083de4e30b417e
javid shabin <javid.main.act@gmail.com>
2025-06-08 00:56:03 +0530

    Add the seller route in the app.ts file

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import express from "express";
 import { globalErrorHandler } from "./middlewares/errorHandler";
 import { userRouter } from "./modules/user";
 import { adminRouter } from "./modules/admin";
+import { sellerRouter } from "./modules/seller";
 
 const app = express();
 
 app.use("/user", userRouter);
 app.use("/admin", adminRouter)
+app.use("/seller", sellerRouter)
 
 // Global error handler
 app.use(globalErrorHandler);
