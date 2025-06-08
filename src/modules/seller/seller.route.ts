6ce37ccbcac096531630ae2bc4b62cf26659055a
javid shabin <javid.main.act@gmail.com>
2025-06-08 11:40:06 +0530

    Create a router for seller otp verification

--- src/modules/seller/seller.route.ts
+++ src/modules/seller/seller.route.ts
@@ -2,10 +2,12 @@ import express from "express";
 import { authenticate } from "../../middlewares/auth.middleware";
 import { authorize } from "../../middlewares/authorize";
 import { upload } from "../../middlewares/multer";
-import { signupSeller } from "./seller.controller";
+import { signupSeller, verifySellerOTP } from "./seller.controller";
 
 const router = express.Router();
 
 // Seller signup
-router.post("/seller-signup", signupSeller)
+router.post("/seller-signup", signupSeller);
+// Seller OTP verification
+router.post("/seller-otp-verification", verifySellerOTP);
 export default router;
