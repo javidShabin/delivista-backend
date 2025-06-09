0153ee974869d269eda8040047812bdd54a4ab9a
javid shabin <javid.main.act@gmail.com>
2025-06-10 01:08:09 +0530

    Set router inside the app.ts file

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,14 @@ import { globalErrorHandler } from "./middlewares/errorHandler";
 import { userRouter } from "./modules/user";
 import { adminRouter } from "./modules/admin";
 import { sellerRouter } from "./modules/seller";
+import { restRouter } from "./modules/restaurant";
 
 const app = express();
 
 app.use("/user", userRouter);
 app.use("/admin", adminRouter)
 app.use("/seller", sellerRouter)
+app.use("/restaurant", restRouter)
 
 // Global error handler
 app.use(globalErrorHandler);
