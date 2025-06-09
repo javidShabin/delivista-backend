1acc9c3d98640f60bfc78ac2682b86e5d6589aad
javid shabin <javid.main.act@gmail.com>
2025-06-10 00:46:21 +0530

    Create service for restaruant controller with cloudinary for store restarurant image

---
+++ src/modules/restaurant/rest.service.ts
@@ -0,0 +1,13 @@
+import cloudinary from "../../configs/cloudinary";
+import { AppError } from "../../utils/appError";
+
+// Handles the restaurant image upload process using Cloudinary
+export const handleAvatarUpload = async (file: any) => {
+  try {
+    const uploadResult = await cloudinary.uploader.upload(file.path);
+    // Store the image url to update user data
+    return uploadResult.secure_url;
+  } catch (error) {
+    throw new AppError("Failed to upload avatar image", 500);
+  }
+};
