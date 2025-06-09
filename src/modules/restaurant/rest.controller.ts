655e87b680ddd160590b0127fbc4dddc795a066c
javid shabin <javid.main.act@gmail.com>
2025-06-10 01:36:27 +0530

    add some commands for creating controller for restaurant

--- src/modules/restaurant/rest.controller.ts
+++ src/modules/restaurant/rest.controller.ts
@@ -3,3 +3,17 @@ import restSchema from "./rest.model";
 import { handleAvatarUpload } from "./rest.service";
 import { AppError } from "../../utils/appError";
 import { validateRestaurantCreation } from "./rest.validation";
+
+// Get all restaurants
+// Get verified restaurants
+// Verification restaurant for admin
+// Get restaurant by ID
+// Get top reated restaurant
+// Filter location based
+// Get restaurant statitics
+// Ge restarant by seller ID
+// Filter restaurant by menu
+// Toggle restaurant status for seller
+// Create restaurant
+// Update restaurant
+// Delete restaurant
