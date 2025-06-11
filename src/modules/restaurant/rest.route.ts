7a35bae8ff9e9acf2e7c8fa307a54bb1733705c9
javid shabin <javid.main.act@gmail.com>
2025-06-11 21:12:42 +0530

    Set the router for get verified restaurant list for custromer and admin

--- src/modules/restaurant/rest.route.ts
+++ src/modules/restaurant/rest.route.ts
@@ -2,7 +2,11 @@ import express from "express";
 import { authenticate } from "../../middlewares/auth.middleware";
 import { authorize } from "../../middlewares/authorize";
 import { upload } from "../../middlewares/multer";
-import { createRestaurant, getAllRestaurants } from "./rest.controller";
+import {
+  createRestaurant,
+  getAllRestaurants,
+  getVerifiedRestaurants,
+} from "./rest.controller";
 
 const router = express.Router();
 
@@ -15,6 +19,13 @@ router.post(
   createRestaurant
 );
 // Get all restaurant list
-router.get("/get-all-restaurants", getAllRestaurants)
+router.get("/get-all-restaurants", getAllRestaurants);
+// Get admin verified restaurant list
+router.get(
+  "/verified-restaurants",
+  authenticate,
+  authorize("customer", "admin"),
+  getVerifiedRestaurants
+);
 
 export default router;
