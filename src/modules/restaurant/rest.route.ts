7e3a0d56ac35e0c56072c22f255a2023a3141399
javid shabin <javid.main.act@gmail.com>
2025-06-12 21:03:51 +0530

    Set the router for get all restaurant list

--- src/modules/restaurant/rest.route.ts
+++ src/modules/restaurant/rest.route.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { authenticate } from "../../middlewares/auth.middleware";
 import { authorize } from "../../middlewares/authorize";
 import { upload } from "../../middlewares/multer";
-import { createRestaurant } from "./rest.controller";
+import { createRestaurant, getAllRestaurants } from "./rest.controller";
 
 const router = express.Router();
 
@@ -14,5 +14,7 @@ router.post(
   upload.single("image"),
   createRestaurant
 );
+// Get all restaurant list
+router.get("/get-all-restaurants", getAllRestaurants)
 
 export default router;
