65a23b53b12e31504c1f9b4b3908eeee1c4720c7
javid shabin <javid.main.act@gmail.com>
2025-06-18 01:03:04 +0530

    Create the menu service page for menu creation if have any image file during the menu creation

--- src/modules/menu/menu.controller.ts
+++ src/modules/menu/menu.controller.ts
@@ -1,5 +1,23 @@
 import { Request, Response, NextFunction } from "express";
-import menuSchema from "./menu.model"
+import menuSchema from "./menu.model";
 import { AppError } from "../../utils/appError";
+import { validateMenuCreation } from "./menu.validation";
 
 // Create menu
+export const createMenu = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    // Validate the details first
+    validateMenuCreation(req.body);
+    // Destructer data from request body after validation
+    const { productName, description, category, price } = req.body;
+    // Check the same item already in db
+    const isMenuItem = await menuSchema.find({ productName });
+    if (isMenuItem) {
+      return next(new AppError("The item already exists", 400));
+    }
+  } catch (error) {}
+};
