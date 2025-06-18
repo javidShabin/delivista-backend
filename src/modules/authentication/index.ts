1fde7f17de3f2e260f4ae6091fa86a9a090363cf
javid shabin <javid.main.act@gmail.com>
2025-06-18 14:22:19 +0530

    Set the index file

---
+++ src/modules/authentication/index.ts
@@ -0,0 +1,17 @@
+import authRouter from "./auth.routes";
+import * as authController from "./auth.controller";
+import * as authService from "./auth.service";
+import * as authModel from "./auth.model";
+import * as tempAuthModel from "./auth.tempModel";
+import * as authInterface from "./auth.interface";
+import * as authValidation from "./auth.validation";
+
+export {
+  authRouter,
+  authController,
+  authService,
+  authModel,
+  tempAuthModel,
+  authInterface,
+  authValidation,
+};
