da6e164e9e4bc37573e082dcde51fefcc69ba07f
javid shabin <javid.main.act@gmail.com>
2025-06-18 17:01:22 +0530

    Remove the all role routing form app.ts file

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,13 @@
 import express from "express";
 import { globalErrorHandler } from "./middlewares/errorHandler";
-// import { userRouter } from "./modules/user";
-import { adminRouter } from "./modules/admin";
-import { sellerRouter } from "./modules/seller";
+
 import { restRouter } from "./modules/restaurant";
 import { authRouter } from "./modules/authentication";
 
 const app = express();
 
 app.use("/authentication", authRouter)
-// app.use("/user", userRouter);
-app.use("/admin", adminRouter)
-app.use("/seller", sellerRouter)
+
 app.use("/restaurant", restRouter)
 
 // Global error handler
