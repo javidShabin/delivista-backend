54d89f88e2355cf1965e935bda0064f030d3b714
javid shabin <javid.main.act@gmail.com>
2025-06-21 00:57:34 +0530

    Make a change

--- src/modules/authentication/auth.routes.ts
+++ src/modules/authentication/auth.routes.ts
@@ -1,18 +1,29 @@
 import express from "express";
 import { authenticate } from "../../middlewares/auth.middleware";
 import { authorize } from "../../middlewares/authorize";
-import { checkUser, loginUser, logoutUser, singupUser, verifyOtpandCreateUser } from "./auth.controller";
+import {
+  checkUser,
+  loginUser,
+  logoutUser,
+  singupUser,
+  verifyOtpandCreateUser,
+} from "./auth.controller";
 
 const router = express.Router();
 
-router.post("/user-signup", singupUser)
+router.post("/user-signup", singupUser);
 
-router.post("/verify-user-otp", verifyOtpandCreateUser)
+router.post("/verify-user-otp", verifyOtpandCreateUser);
 
-router.post("/user-login", loginUser)
+router.post("/user-login", loginUser);
 
-router.get("/verify-auth", authenticate, authorize("admin","customer","seller"), checkUser)
+router.get(
+  "/verify-auth",
+  authenticate,
+  authorize("admin", "customer", "seller"),
+  checkUser
+);
 
-router.delete("/user-logout", logoutUser)
+router.delete("/user-logout", logoutUser);
 
 export default router;
