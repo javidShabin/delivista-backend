b76d84408d163e82db5f5cada6e60eef7839405c
javid shabin <javid.main.act@gmail.com>
2025-06-21 00:55:45 +0530

    Create the route for user authentication checking controller and test by postman

--- src/modules/authentication/auth.routes.ts
+++ src/modules/authentication/auth.routes.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { authenticate } from "../../middlewares/auth.middleware";
 import { authorize } from "../../middlewares/authorize";
-import { loginUser, logoutUser, singupUser, verifyOtpandCreateUser } from "./auth.controller";
+import { checkUser, loginUser, logoutUser, singupUser, verifyOtpandCreateUser } from "./auth.controller";
 
 const router = express.Router();
 
@@ -10,6 +10,8 @@ router.post("/user-signup", singupUser)
 router.post("/verify-user-otp", verifyOtpandCreateUser)
 
 router.post("/user-login", loginUser)
+
+router.get("/verify-auth", authenticate, authorize("admin","customer","seller"), checkUser)
 
 router.delete("/user-logout", logoutUser)
 
