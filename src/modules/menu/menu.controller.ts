f3748e104b38adfd19f923048863eb3f3e8e171c
javid shabin <javid.main.act@gmail.com>
2025-07-06 00:55:34 +0530

    Create menu controller function needed function decleraion onluy and export all function inluded try catch method

--- src/modules/menu/menu.controller.ts
+++ src/modules/menu/menu.controller.ts
@@ -3,21 +3,135 @@ import menuSchema from "./menu.model";
 import { AppError } from "../../utils/appError";
 import { validateMenuCreation } from "./menu.validation";
 
-// Create menu
+// *************Main Menu CRUD Operations********************
+// Create a new menu item
 export const createMenu = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   try {
-    // Validate the details first
-    validateMenuCreation(req.body);
-    // Destructer data from request body after validation
-    const { productName, description, category, price } = req.body;
-    // Check the same item already in db
-    const isMenuItem = await menuSchema.find({ productName });
-    if (isMenuItem) {
-      return next(new AppError("The item already exists", 400));
-    }
+  } catch (error) {}
+};
+
+// updateMenu
+export const updateMenu = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+  } catch (error) {}
+};
+
+// deleteMenu
+export const deleteMenu = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+  } catch (error) {}
+};
+
+// ************Get menus by association**********************
+// getMenusByRestaurant
+export const getMenusByRestaurant = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+  } catch (error) {}
+};
+
+// getMenusBySeller
+export const getMenusBySeller = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+  } catch (error) {}
+};
+
+// ************Filtering functions for menus**********************
+// getMenusByCategory
+export const getMenusByCategory = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+  } catch (error) {}
+};
+
+// getMenusByPriceRange
+export const getMenusByPriceRange = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+  } catch (error) {}
+};
+
+// getMenusByTag
+export const getMenusByTag = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+  } catch (error) {}
+};
+
+// getMenusByAvailability
+export const getMenusByAvailability = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+  } catch (error) {}
+};
+
+// getRecommendedMenus
+export const getRecommendedMenus = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+  } catch (error) {}
+};
+
+// getVegMenus
+export const getVegMenus = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+  } catch (error) {}
+};
+
+// getNonVegMenus
+export const getNonVegMenus = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+  } catch (error) {}
+};
+
+// get menus by search
+export const searchMenus = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
   } catch (error) {}
 };
