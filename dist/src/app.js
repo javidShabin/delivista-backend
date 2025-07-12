f9ad14cd894f1508cd2705dc1a04477b56a859b6
javid shabin <javid.main.act@gmail.com>
2025-07-12 18:31:41 +0530

    Upadate the dist last commit

--- dist/src/app.js
+++ dist/src/app.js
@@ -8,10 +8,12 @@ const errorHandler_1 = require("./middlewares/errorHandler");
 const restaurant_1 = require("./modules/restaurant");
 const authentication_1 = require("./modules/authentication");
 const user_1 = require("./modules/user");
+const menu_1 = require("./modules/menu");
 const app = (0, express_1.default)();
 app.use("/authentication", authentication_1.authRouter);
 app.use("/user", user_1.userRouter);
 app.use("/restaurant", restaurant_1.restRouter);
+app.use("/menu", menu_1.menuRouter);
 // Global error handler
 app.use(errorHandler_1.globalErrorHandler);
 exports.default = app;
--- dist/src/modules/menu/menu.route.js
+++ dist/src/modules/menu/menu.route.js
@@ -4,5 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const auth_middleware_1 = require("../../middlewares/auth.middleware");
+const authorize_1 = require("../../middlewares/authorize");
+const multer_1 = require("../../middlewares/multer");
+const menu_controller_1 = require("./menu.controller");
 const router = express_1.default.Router();
+router.post("/create-menu", auth_middleware_1.authenticate, (0, authorize_1.authorize)("seller"), multer_1.upload.single("image"), menu_controller_1.createMenu);
 exports.default = router;
