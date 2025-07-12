a1d9964e6bf96819c0fa2d53065edc54a210a364
javid shabin <javid.main.act@gmail.com>
2025-07-12 18:13:08 +0530

    Create the index and router for menu and configue it

---
+++ src/modules/menu/index.ts
@@ -0,0 +1,7 @@
+import menuRouter from "./menu.route"
+import * as menuController from "./menu.controller"
+
+export {
+    menuRouter,
+    menuController
+}
---
+++ src/modules/menu/menu.route.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import { authenticate } from "../../middlewares/auth.middleware";
+import { authorize } from "../../middlewares/authorize";
+import { upload } from "../../middlewares/multer";
+import { createMenu } from "./menu.controller";
+
+const router = express.Router();
+
+export default router;
