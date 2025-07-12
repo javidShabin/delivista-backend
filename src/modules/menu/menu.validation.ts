6ede0d3b841091cf556f98345bb4b07a30f8b982
javid shabin <javid.main.act@gmail.com>
2025-07-12 18:11:42 +0530

    Update the menu validation also include the seller and restaurant id

--- src/modules/menu/menu.validation.ts
+++ src/modules/menu/menu.validation.ts
@@ -4,9 +4,9 @@ import { IMenuCreation } from "./menu.interface";
 // Validation for menu creation
 export const validateMenuCreation = (data: IMenuCreation) => {
   // Destructer menu creation date from interface
-  const { productName, description, category, price } = data;
+  const { productName, description, category, price, sellerId, restaurantId } = data;
   // Chexk the required fields are present or not
-  if (!productName || !description || !category || !price) {
+  if (!productName || !description || !category || !price || !sellerId || !restaurantId) {
     throw new AppError("All fields are required", 400);
   }
 };
