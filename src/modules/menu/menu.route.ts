c8fc5af119d41c7c0df51bf6f284081e1c99313b
javid shabin <javid.main.act@gmail.com>
2025-07-14 02:06:21 +0530

    Creat the menu list geting controller and add the pagination there , also add route for the controller functon

--- src/modules/menu/menu.route.ts
+++ src/modules/menu/menu.route.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { authenticate } from "../../middlewares/auth.middleware";
 import { authorize } from "../../middlewares/authorize";
 import { upload } from "../../middlewares/multer";
-import { createMenu } from "./menu.controller";
+import { createMenu, getMenusByRestaurant } from "./menu.controller";
 
 const router = express.Router();
 
@@ -12,6 +12,13 @@ router.post(
   authorize("seller"),
   upload.single("image"),
   createMenu
+);
+
+router.get(
+  "/get-all-menus/:restaurantId",
+  authenticate,
+  authorize("admin", "customer"),
+  getMenusByRestaurant
 );
 
 export default router;
