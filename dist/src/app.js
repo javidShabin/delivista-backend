3e317f20370ca34e962b051e6a7963f87a07a730
javid shabin <javid.main.act@gmail.com>
2025-07-15 15:44:54 +0530

    Update the last commits to dist folder

--- dist/src/app.js
+++ dist/src/app.js
@@ -9,11 +9,13 @@ const restaurant_1 = require("./modules/restaurant");
 const authentication_1 = require("./modules/authentication");
 const user_1 = require("./modules/user");
 const menu_1 = require("./modules/menu");
+const cart_1 = require("./modules/cart");
 const app = (0, express_1.default)();
 app.use("/authentication", authentication_1.authRouter);
 app.use("/user", user_1.userRouter);
 app.use("/restaurant", restaurant_1.restRouter);
 app.use("/menu", menu_1.menuRouter);
+app.use("/cart", cart_1.cartRouter);
 // Global error handler
 app.use(errorHandler_1.globalErrorHandler);
 exports.default = app;
