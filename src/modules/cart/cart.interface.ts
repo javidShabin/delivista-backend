60d98a4a2f44f7c5031e2515563027773a06310d
javid shabin <javid.main.act@gmail.com>
2025-07-16 01:07:00 +0530

    Update the interface for cart add new interface for creating cart, and remove subcart interface

--- src/modules/cart/cart.interface.ts
+++ src/modules/cart/cart.interface.ts
@@ -1,24 +1,42 @@
 import { Types } from "mongoose";
 
-// Subschema for cart items
-interface ICartItem {
-  menuItem: Types.ObjectId;
-  quantity: number;
-  price: number;
-  image?: string;
-  productName: string;
-  category: string;
-  isVeg: boolean;
-}
-
 // Cart main schema interface
 export interface ICart {
   sellerId: Types.ObjectId;
   customerId: Types.ObjectId;
   restaurantId: Types.ObjectId;
-  items: ICartItem[];
+  items: [
+    {
+      menuId: Types.ObjectId;
+      quantity: number;
+      price: number;
+      image?: string;
+      productName: string;
+      category: string;
+      isVeg: boolean;
+    }
+  ];
   totalPrice: number;
   orderStatus: "pending" | "in-progress" | "completed";
   createdAt: Date;
   updatedAt: Date;
+}
+
+// Create cart interface
+export interface ICartCreation {
+  sellerId: Types.ObjectId;
+  customerId: Types.ObjectId;
+  restaurantId: Types.ObjectId;
+  items: [
+    {
+      menuId: Types.ObjectId;
+      quantity: number;
+      price: number;
+      image?: string;
+      productName: string;
+      category: string;
+      isVeg: boolean;
+    }
+  ];
+  totalPrice: number;
 }
