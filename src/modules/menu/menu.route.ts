5facec7d3c869e2582a3cdb0b87b152480451e67
javid shabin <javid.main.act@gmail.com>
2025-07-15 12:22:21 +0530

    Add route for getin availble menu items ,  and add commands for all routes

--- src/modules/menu/menu.route.ts
+++ src/modules/menu/menu.route.ts
@@ -2,10 +2,22 @@ import express from "express";
 import { authenticate } from "../../middlewares/auth.middleware";
 import { authorize } from "../../middlewares/authorize";
 import { upload } from "../../middlewares/multer";
-import { createMenu, deleteMenu, getMenusByCategory, getMenusByRestaurant, getMenusByTag, searchMenus, updateMenu } from "./menu.controller";
+import {
+  createMenu,
+  deleteMenu,
+  getMenusByAvailability,
+  getMenusByCategory,
+  getMenusByRestaurant,
+  getMenusByTag,
+  searchMenus,
+  updateMenu,
+} from "./menu.controller";
 
 const router = express.Router();
 
+// ****************** Endpoints for Menu Management ****************** //
+
+// Route for create new menu by seller
 router.post(
   "/create-menu",
   authenticate,
@@ -14,14 +26,16 @@ router.post(
   createMenu
 );
 
+// Route for update existing menu by id by seller
 router.put(
   "/update-menu/:id",
   authenticate,
   authorize("seller"),
   upload.single("image"),
   updateMenu
-)
+);
 
+// Route for delete existing menu by id by seller
 router.delete(
   "/remove-menu/:id",
   authenticate,
@@ -29,6 +43,7 @@ router.delete(
   deleteMenu
 );
 
+// Route for get all menus by restaurant by admin or customer
 router.get(
   "/get-all-menus/:restaurantId",
   authenticate,
@@ -36,6 +51,7 @@ router.get(
   getMenusByRestaurant
 );
 
+// Route for get all menus by category by admin or customer
 router.get(
   "/get-menu-by-catagory/:category",
   authenticate,
@@ -43,14 +59,23 @@ router.get(
   getMenusByCategory
 );
 
+// Route for get all menus by tag by admin or customer
 router.get(
   "/menu-by-tag",
   authenticate,
   authorize("admin", "customer"),
   getMenusByTag
-)
+);
 
+// Route for get all menus by availability by admin or customer (available or unavailable) by admin or customer
+router.get(
+  "/available-menu",
+  authenticate,
+  authorize("admin", "customer"),
+  getMenusByAvailability
+);
 
+// Route for search menus by name by admin or customer
 router.get(
   "/search-menu",
   authenticate,
