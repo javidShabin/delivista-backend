ebf83f7a82065dbbf1413d2fe568ef693160c08a
javid shabin <javid.main.act@gmail.com>
2025-07-25 02:40:01 +0530

    Remove unwanted commend

--- src/modules/wishlist/wishlist.route.ts
+++ src/modules/wishlist/wishlist.route.ts
@@ -11,13 +11,5 @@ const router = express.Router();
 // Add item to wishlist
 router.post("/add", authenticate, authorize("customer"), addToWishlist);
 
-// // Get all wishlist items by user ID
-// router.get("/items", authenticate, authorize("customer"), getAllWishlistByUserId);
-
-// // Remove item from wishlist
-// router.delete("/remove", authenticate, authorize("customer"), removeWishlistItem);
-
-// // Check if item exists in wishlist (optional)
-// router.get("/check", authenticate, authorize("customer"), checkWishlistItem);
 
 export default router;
