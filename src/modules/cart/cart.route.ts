192d49e7c15c33e783ac60cf6e5ecceda264e870
javid shabin <javid.main.act@gmail.com>
2025-08-03 00:58:51 +0530

    Set the route for remove cart

--- src/modules/cart/cart.route.ts
+++ src/modules/cart/cart.route.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import { authenticate } from "../../middlewares/auth.middleware";
 import { authorize } from "../../middlewares/authorize";
-import { addToCart, getCartByUserId } from "./cart.controller";
+import { addToCart, deleteFromCart, getCartByUserId } from "./cart.controller";
 
 const router = express.Router();
 
 router.post("/add-cart", authenticate, authorize("customer"), addToCart);
 router.get("/cart-items", authenticate, authorize("customer"), getCartByUserId);
+router.delete(
+  "/remove-item",
+  authenticate,
+  authorize("customer"),
+  deleteFromCart
+);
 
 export default router;
