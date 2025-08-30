503194e8a678d4075dcae849284ded6a179fe955
javid shabin <javid.main.act@gmail.com>
2025-08-31 01:47:00 +0530

    Test the pyment getway in locally its working well its navigete to stripe page and see the price and details

--- src/modules/payment/pyment.controller.ts
+++ src/modules/payment/pyment.controller.ts
@@ -25,7 +25,7 @@ export const makePayment = async (req: Request, res: Response) => {
                         name: item.productName,
                         images: [item.image],
                     },
-                    unit_amount: item.price * 100,
+                    unit_amount: Math.round(item.price * 1.05 * 100),
                 },
                 quantity: item.quantity,
             })),
