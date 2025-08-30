9b0703b9893ac8fc9d3ab19515490041aec45044
javid shabin <javid.main.act@gmail.com>
2025-08-31 02:08:03 +0530

    make a change

--- src/configs/stripe.ts
+++ src/configs/stripe.ts
@@ -1,6 +1,9 @@
 import Stripe from "stripe";
 
+if (!process.env.STRIPE_SECRET_KEY) {
+    throw new Error("Stripe secret key is missing! Add STRIPE_SECRET_KEY to environment variables.");
+}
+
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-    apiVersion: "2024-06-20" as any,
-  });
-  
+    apiVersion: "2025-07-30.basil", // valid version from Stripe types
+});
