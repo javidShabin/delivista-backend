8c61ceaea044ad61bf2bdb66765998b2ddf3dade
javid shabin <javid.main.act@gmail.com>
2025-08-31 02:18:22 +0530

    Trying to solve error

--- src/configs/stripe.ts
+++ src/configs/stripe.ts
@@ -4,4 +4,6 @@ if (!process.env.STRIPE_SECRET_KEY) {
     throw new Error("Stripe secret key is missing! Add STRIPE_SECRET_KEY to environment variables.");
 }
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+    apiVersion: "2025-07-30.basil",
+});
